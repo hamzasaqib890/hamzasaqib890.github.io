3883305e292f32b28236a5c0092cdc2424ccdeb9
Hamza Saqib <chaosemerald890@gmail.com>
2022-09-15 16:20:03 -0400

    removed magnify

--- js/pixels.js
+++ js/pixels.js
@@ -40,47 +40,16 @@ class Pixel {
     this.color = `hsl(${this.hue}, 100%, 50%)`;
   }
 
-  draw(horTotalOffset, verTotalOffset, magnifyX, magnifyY) {
+  draw() {
     const y = this.y + 1300 - scrollY/5;
-    //const y = this.y
     if (y < -50 || y > pixelsCanvas.height + 50) {
       return;
     }
 
-    const dist = Math.sqrt((magnifyX - this.x) ** 2 + (magnifyY - y) ** 2);
-    const length = (1 - Math.min(dist, 250) / 250) * 20 + 6;
+    const length = 6;
 
-    if (length === 6) {
-      pixelsctx.fillStyle = this.color;
-      pixelsctx.fillRect(this.x - length / 2, y - length / 2, length, length);
-    } else {
-      const horOffset = (1 - dist / 250) * horTotalOffset;
-      const verOffset = (1 - dist / 250) * verTotalOffset;
-
-      const rgb = hslToRgb(this.hue / 360, 1, 0.5);
-
-      pixelsctx.fillStyle = `hsl(360, 100%, ${(rgb[0] / 255 / 4) * 100 + 25}%)`;
-      pixelsctx.fillRect(
-        this.x - length / 2 + horOffset,
-        y - length / 2 + verOffset,
-        length / 3,
-        length
-      );
-      pixelsctx.fillStyle = `hsl(120, 100%, ${(rgb[1] / 255 / 4) * 100 + 25}%)`;
-      pixelsctx.fillRect(
-        this.x - length / 2 + length / 3 + horOffset,
-        y - length / 2 + verOffset,
-        length / 3,
-        length
-      );
-      pixelsctx.fillStyle = `hsl(240, 100%, ${(rgb[2] / 255 / 4) * 100 + 25}%)`;
-      pixelsctx.fillRect(
-        this.x - length / 2 + (2 * length) / 3 + horOffset,
-        y - length / 2 + verOffset,
-        length / 3,
-        length
-      );
-    }
+    pixelsctx.fillStyle = this.color;
+    pixelsctx.fillRect(this.x - length / 2, y - length / 2, length, length);
   }
 }
 
@@ -91,18 +60,6 @@ export class PixelsAnimation {
   constructor() {
     this.setSize();
     addEventListener("resize", () => this.setSize());
-
-    this.magnify = {
-      x: 100,
-      y: 400,
-      radius: 250,
-      width: 15,
-    };
-
-    addEventListener("scroll", () => {
-      this.magnify.x = scrollY * 1.4 + 100;
-      this.magnify.y = scrollY * 0.5 + 400;
-    });
 
     this.anim();
   }
@@ -117,7 +74,7 @@ export class PixelsAnimation {
     this.pixelArray = [];
 
     for (let x = 0; x < pixelsCanvas.width; x += this.pixelGap) {
-      for (let y = 0; y < pixelsCanvas.height * 4; y += this.pixelGap) {
+      for (let y = 0; y < pixelsCanvas.height * 5; y += this.pixelGap) {
         this.pixelArray.push(new Pixel(x, y, ((x) / 10) % 360));
       }
     }
@@ -126,39 +83,15 @@ export class PixelsAnimation {
   drawBackground() {
     pixelsctx.clearRect(0, 0, pixelsCanvas.width, pixelsCanvas.height);
 
-    const horTotalOffset = (this.magnify.x - pixelsCanvas.width / 2) / 15;
-    const verTotalOffset = (this.magnify.y - innerHeight / 2) / 15;
-
     for (let pixel of this.pixelArray) {
       pixel.updateColor();
-      pixel.draw(
-        horTotalOffset,
-        verTotalOffset,
-        this.magnify.x,
-        this.magnify.y
-      );
+      pixel.draw();
     }
-  }
-
-  drawMagnify() {
-    pixelsctx.strokeStyle = "rgb(255, 255, 255)";
-    pixelsctx.lineWidth = this.magnify.width;
-    pixelsctx.beginPath();
-    pixelsctx.arc(
-      this.magnify.x,
-      this.magnify.y,
-      this.magnify.radius,
-      0,
-      Math.PI * 2,
-      true
-    );
-    pixelsctx.stroke();
   }
 
   anim() {
     requestAnimationFrame(() => this.anim());
 
     this.drawBackground();
-    this.drawMagnify();
   }
 }
